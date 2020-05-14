c2f10c2696ab3cd5044f66731a983a8296271e65
Sergei Kolesnikov <win0err@gmail.com>
2020-05-14 18:07:09 +0300

    Add solutions to Single Number problem

---
+++ leetcode/algorithms/single-number.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def bits(self, nums: List[int]) -> int:
+        missing = 0
+
+        for n in nums:
+            missing ^= n
+            
+        return missing
+
+    def math(self, nums: List[int]) -> int:
+        return 2 * sum(set(nums)) - sum(nums)
