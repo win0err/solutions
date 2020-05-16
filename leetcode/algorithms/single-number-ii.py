1201ceb2e20d2423cf2d8fc9d247d6d9633349b1
Sergei Kolesnikov <win0err@gmail.com>
2020-05-16 22:47:25 +0300

    Add hashmap solution to Single Number II

---
+++ leetcode/algorithms/single-number-ii.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+class Solution:
+    def hashmap(self, nums: List[int]) -> int:
+        uniq = defaultdict(int)
+
+        for n in nums:
+            uniq[n] += 1
+        
+        for i in uniq:
+            if uniq[i] != 3:
+                return i
