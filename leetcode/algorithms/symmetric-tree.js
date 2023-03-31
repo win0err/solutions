04c444c10eaca7aced54711c6d62af7cd7ff39b4
Sergei Kolesnikov <sergei@kolesnikov.se>
2023-03-31 13:04:10 +0300

    Add solution to problem 101

---
+++ leetcode/algorithms/symmetric-tree.js
@@ -0,0 +1,17 @@
+const isSymmetric = ({ left, right }) => {
+	const queue = [[left, right]]
+
+	while (queue.length > 0) {
+		const [l, r] = queue.shift()
+
+		if (!l && !r)
+			continue
+
+		if (l?.val !== r?.val)
+			return false
+
+		queue.push([l.left, r.right], [l.right, r.left])
+	}
+
+	return true
+}
