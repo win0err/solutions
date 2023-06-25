aecb7c170544d8124af125a2a632500b9d865c26
Sergei Kolesnikov <sergei@kolesnikov.se>
2023-06-25 16:44:58 +0300

    Add another approach to insertion sort

--- books/clrs/ch1/insertion-sort/solution.js
+++ books/clrs/ch1/insertion-sort/solution.js
@@ -1,9 +1,9 @@
-const insertionSort = (list) => {
+const insertionSortFromBook = (list) => {
     for (let i = 1; i < list.length; i++) {
         let current = list[i]
 
         let j
-        for (j = i-1; j >= 0 && list[j] >= current; j--) {
+        for (j = i-1; j >= 0 && list[j] > current; j--) {
             list[j+1] = list[j];
         }
 
@@ -11,8 +11,23 @@ const insertionSort = (list) => {
     }
 }
 
+const insertionSort = (list) => {
+    for (let i = 1; i < list.length; i++) {
+        for (let j = i; j > 0 && list[j-1] > list[j]; j--) {
+            [list[j], list[j-1]] = [list[j-1], list[j]]
+        }
+    }
+}
 
-const list = [-2, 99, 0, -743, 2, 3, 4]
-insertionSort(list)
+const unsorted= [-2, 99, 0, -743, 2, 3, 4]
 
-console.log(JSON.stringify(list))
+const list1 = Array.from(unsorted)
+insertionSortFromBook(list1)
+
+console.log(JSON.stringify(list1))
+
+
+const list2 = Array.from(unsorted)
+insertionSort(list2)
+
+console.log(JSON.stringify(list2))
