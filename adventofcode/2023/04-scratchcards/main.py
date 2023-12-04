41b83178d0ba07a7eec3f9764f0a4a3306272fda
Sergei Kolesnikov <sergei@kolesnikov.se>
2023-12-04 15:09:09 +0300

    Add AoC 2023 - Day 4 solution

---
+++ adventofcode/2023/04-scratchcards/main.py
@@ -0,0 +1,44 @@
+def parse_input(filename):
+    cards = []
+
+    with open(filename, 'r+') as f:
+        for line in f:
+            if line == '\n':
+                continue
+
+            _, numbers = line.strip().split(': ')
+            numbers_have, numbers_win = map(
+                lambda nums: set(int(n) for n in nums.split(' ') if n != ''),
+                numbers.split(' | ')
+            )
+
+            matches = numbers_have & numbers_win
+            cards.append(len(matches))
+
+    return cards
+
+
+def solve_1(cards):
+    total_score = 0
+
+    for matching_count in cards:
+        total_score += int(2**(matching_count - 1))
+
+    return total_score
+
+
+def solve_2(cards):
+    cards_count = [1] * len(cards)
+
+    for idx, matching_count in enumerate(cards):
+        for i in range(matching_count):
+            cards_count[idx + 1 + i] += cards_count[idx]
+
+    return sum(cards_count)
+
+
+if __name__ == '__main__':
+    cards = parse_input('input.txt')
+
+    print(f'Task 1: {solve_1(cards)}')
+    print(f'Task 2: {solve_2(cards)}')
